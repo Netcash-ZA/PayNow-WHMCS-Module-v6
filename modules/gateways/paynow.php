67382304eefe7ad1092f979e009e7c468e413633
Jurgens Banninga <jurgens@interexcel.co.za>
2015-02-18 08:58:19 +0200

    Some whitespace issues

--- modules/gateways/paynow.php
+++ modules/gateways/paynow.php
@@ -6,12 +6,12 @@ function paynow_config() {
 	$configarray = array (
 			"FriendlyName" => array (
 					"Type" => "System",
-					"Value" => "Sage Pay Now" 
+					"Value" => "Sage Pay Now"
 			),
 			"servicekey" => array (
 					"FriendlyName" => "Service Key",
 					"Type" => "text",
-					"Size" => "40" 
+					"Size" => "40"
 			),
             "whmcs_admin_username" => array (
                 "FriendlyName" => "WHMCS Admin User Name",
@@ -22,24 +22,24 @@ function paynow_config() {
 			"sendemailconfirm" => array (
 					"FriendlyName" => "Send email",
 					"Type" => "yesno",
-					"Description" => "An email confirmation will be sent from the Pay Now gateway to the client after each transaction." 
-			) 
+					"Description" => "An email confirmation will be sent from the Pay Now gateway to the client after each transaction."
+			)
 	);
 	return $configarray;
 }
 function paynow_link($params) {
-	
+
 	// Gateway Specific Variables
 	$m1_PayNowServiceKey = $params ['servicekey'];
 	// Software vendor key is the hard coded for Pay Now Online web software requests
 	$m2_SoftwareVendorKey = "24ade73c-98cf-47b3-99be-cc7b867b3080";
-	
+
 	// Invoice Variables
 	$p2_UniqueRef = sprintf("%d-%d", $params ['invoiceid'], date("U"));
 	$p3_Description = $params ['description'];
 	$p4_Amount = $params ['amount'];
 	$Budget = "N";
-	
+
 	// Client details
 	$m4_Extra1 = $params ['clientdetails'] ['userid'];
 	$m5_Extra2 = $params ['clientdetails'] ['firstname'] . ' ' . $params ['clientdetails'] ['firstname'];
@@ -47,18 +47,18 @@ function paynow_link($params) {
 		$m5_Extra2 = $m5_Extra2 . $params ['clientdetails'] ['companyname'];
 	}
 	$m6_Extra3 = $params ['clientdetails'] ['phonenumber'];
-	
+
 	if ($params ['sendemailconfirm'] == 'on') {
 		$m9_CardHolder = $params ['clientdetails'] ['email'];
 	}
-	
+
 	$m10_ReturnText = "GatewayReturned";
-	
+
 	$invoiceid = $params ['invoiceid'];
 	$description = $params ["description"];
 	$amount = $params ['amount'];
 	$currency = $params ['currency'];
-	
+
 	// Gateway submit code
 	// Refer to documentation
 	$code = '<form action="https://paynow.sagepay.co.za/site/paynow.aspx" method="post">
@@ -75,8 +75,6 @@ function paynow_link($params) {
 				<input type="hidden" name="m10" value="' . $m10_ReturnText . '" />
 <input type="submit" value="Pay" />
 </form>';
-	
+
 	return $code;
 }
-
-?>
