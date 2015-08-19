55df42f95c2c65a681cc51439ba4178f7c8315c5
Jurgens Banninga <jurgens@interexcel.co.za>
2015-08-19 16:02:49 +0200

    Differentiate between redirects, notify and CC payments

--- modules/gateways/callback/paynow.php
+++ modules/gateways/callback/paynow.php
@@ -42,56 +42,8 @@ function pnLog( $msg = '', $close = false ) {
     }
 }
 
-pnLog( 'Callback Received: '. print_r( $_POST, true ) );
-
-$gatewaymodule = "paynow"; # Enter your gateway module name here replacing template
-
-$GATEWAY = getGatewayVariables($gatewaymodule);
-if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
-
-pnLog( 'GATEWAY: '. print_r( $GATEWAY, true ) );
-
-# Get Returned Variables - Adjust for Post Variable Names from your Gateway's Documentation
-$status = $_POST["TransactionAccepted"];
-
-// Reference is sent as p2
-$matches = array();
-preg_match('/(\d{1,4})-/', $_POST["Reference"], $matches);
-
-$invoiceid = $matches[1];
-$transid = $_POST["RequestTrace"];
-$amount = $_POST["Amount"];
-$fee = "";
-$adminuser = $GATEWAY['api_user'];
-
-$invoiceid = checkCbInvoiceID($invoiceid,$GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
-
-pnLog( 'Invoice Id: '. print_r( $invoiceid, true ));
-pnLog( 'Status: '. print_r( $status, true ) . ' - ' .  gettype($status) );
-
-checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
-pnLog( 'checkCbTransID Called' );
-
-if ($status=="true") {
-    # Successful
-    pnLog( 'Transaction Successful' );
-
-    // addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
-    $command = "addinvoicepayment";
-    $values = array();
-    $values["invoiceid"] = $invoiceid;
-    $values["transid"] = $transid;
-    $values["amount"] = $amount;
-    $values["fee"] = $fee;
-    $values["gateway"] = $GATEWAY['name'];
-    $values["date"] = $GATEWAY['name'];
-    $results = localAPI($command,$values,$adminuser);
-
-    pnLog( 'addinvoicepayment Result: '. print_r( $results, true ) );
-
-    logTransaction($GATEWAY["name"],$_POST,"Successful"); # Save to Gateway Log: name, data array, status
-
-    echo "<p>Payment was successful.</p>";
+function pn_redirect() {
+    echo "<p>We're awaiting the status of your payment.</p>";
     echo "<p>You will be redirected to the client area in 5 seconds. <a href='../../../clientarea.php'>Click here</a> to return immediately.</p>";
     ?>
 
@@ -102,13 +54,102 @@ if ($status=="true") {
     </script>
     <?php
     pnLog( 'Returning to clientarea.' );
-} else {
-    # Unsuccessful
-    pnLog( 'Transaction Unsuccessful' );
-    logTransaction($GATEWAY["name"],$_POST,"Unsuccessful"); # Save to Gateway Log: name, data array, status
-    echo "<p>Payment was declined. Reason: " . $_POST['Reason'] . "</p>";
-    echo "<p><a href='../../../cart.php'>Click here</a> to return to the cart.</p>";
 }
 
-pnLog( 'Completed' );
-pnLog( '', true );
+function pn_do_transaction() {
+
+    pnLog( 'Callback Received: '. print_r( $_POST, true ) );
+
+    $gatewaymodule = "paynow"; # Enter your gateway module name here replacing template
+
+    $GATEWAY = getGatewayVariables($gatewaymodule);
+    if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
+
+    pnLog( 'GATEWAY: '. print_r( $GATEWAY, true ) );
+
+    # Get Returned Variables - Adjust for Post Variable Names from your Gateway's Documentation
+    $status = $_POST["TransactionAccepted"];
+
+    // Reference is sent as p2
+    $matches = array();
+    preg_match('/(\d{1,4})-/', $_POST["Reference"], $matches);
+
+    $invoiceid = $matches[1];
+    $transid = $_POST["RequestTrace"];
+    $amount = $_POST["Amount"];
+    $fee = "";
+    $adminuser = $GATEWAY['api_user'];
+
+    $invoiceid = checkCbInvoiceID($invoiceid,$GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
+
+    pnLog( 'Invoice Id: '. print_r( $invoiceid, true ));
+    pnLog( 'Status: '. print_r( $status, true ) . ' - ' .  gettype($status) );
+
+    checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
+    pnLog( 'checkCbTransID Called' );
+
+    if ($status=="true") {
+        # Successful
+        pnLog( 'Transaction Successful' );
+
+        // addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+        $command = "addinvoicepayment";
+        $values = array();
+        $values["invoiceid"] = $invoiceid;
+        $values["transid"] = $transid;
+        $values["amount"] = $amount;
+        $values["fee"] = $fee;
+        $values["gateway"] = $GATEWAY['name'];
+        $values["date"] = $GATEWAY['name'];
+        $results = localAPI($command,$values,$adminuser);
+
+        pnLog( 'addinvoicepayment Result: '. print_r( $results, true ) );
+
+        logTransaction($GATEWAY["name"],$_POST,"Successful"); # Save to Gateway Log: name, data array, status
+
+        echo "<p>Payment was successful.</p>";
+        echo "<p>You will be redirected to the client area in 5 seconds. <a href='../../../clientarea.php'>Click here</a> to return immediately.</p>";
+        ?>
+
+        <script type="text/javascript">
+            setTimeout(function () {
+               window.location.href = "../../../clientarea.php";
+            }, 5000);
+        </script>
+        <?php
+        pnLog( 'Returning to clientarea.' );
+    } else {
+        # Unsuccessful
+        pnLog( 'Transaction Unsuccessful' );
+        logTransaction($GATEWAY["name"],$_POST,"Unsuccessful"); # Save to Gateway Log: name, data array, status
+        echo "<p>Payment was declined. Reason: " . $_POST['Reason'] . "</p>";
+        echo "<p><a href='../../../cart.php'>Click here</a> to return to the cart.</p>";
+    }
+
+    pnLog( 'Completed' );
+    pnLog( '', true );
+}
+
+if( isset($_POST) && !empty($_POST) ) {
+
+    // This is the notification coming in!
+    // Act as an IPN request and forward request to Credit Card method.
+    // Logic is exactly the same
+
+    pn_do_transaction();
+    die();
+
+} else {
+    // Probably calling the "redirect" URL
+
+    pnlog(__FILE__ . ' Probably calling the "redirect" URL');
+
+    if( $url_for_redirect ) {
+        pn_redirect();
+        // header ( "Location: {$url_for_redirect}" );
+    } else {
+        die( "No 'redirect' URL set." );
+    }
+}
+
+die( PN_ERR_BAD_ACCESS );
